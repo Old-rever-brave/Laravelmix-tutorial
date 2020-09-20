191a91723d825193774f5ba097b294de790d8b28
old.rever.brave <54588747+Old-rever-brave@users.noreply.github.com>
2020-09-20 19:24:10 +0900

    Update create.blade.php

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -16,13 +16,13 @@
 
    {{ csrf_field() }}
    
-   <tr><th>やってみたい分野</th><td>{{Form::select('category', [ 'インフラ' => 'インフラ', 'マネジメント' => 'マネジメント', 'backend' =>'backend', 'frontend' =>'frontend'])}}</td></tr>
-   <tr><th>バックエンド技術</th><td>{{Form::select('fulstack', ['Play framework' => 'Play framework', 'Laravel' => 'Laravel', 'Django' =>'Django', 'Express js' =>'Express js', 'RubyonRails' =>'RubyonRails','Play' =>'Play','Sym fony'=>'Sym fony', 'Cake PHP'=>'Cake PHP', 'Angular'=>'Angular'])}}</td></tr>
+   <tr><th>やってみたい分野</th><td>{{Form::select('category', [ 'インフラ' => 'インフラ', '上流工程' => '上流工程', 'サーバーサイド' =>'サーバーサイド', 'フロントエンド' =>'フロントエンド'])}}</td></tr>
+   <tr><th>バックエンド技術</th><td>{{Form::select('fulstack', ['Play framework' => 'Play framework', 'Laravel' => 'Laravel', 'Django' =>'Django', 'Express js' =>'Express js', 'RubyonRails' =>'RubyonRails','Sym fony'=>'Sym fony', 'Cake PHP'=>'Cake PHP', 'Angular'=>'Angular'])}}</td></tr>
    <tr><th>フロントエンド技術</th><td>{{Form::select('frontend', ['React js' => 'React js', 'Vue.js' => 'Vue.js', 'Angular' =>'Angular','Ember js' =>'Ember js','boot strap' =>'boot strap'])}}</td></tr>
    <tr><th>使用したいデータベース</th><td>{{Form::select('sqls', ['mysql' => 'mysql', 'sqlite' => 'sqlite', 'firebase' => 'firebase'])}}</td></tr>
    <tr><th>使用したい言語</th><td>{{Form::select('gengo', ['Java' => 'Java', 'php' => 'php', 'C' =>'C', 'C++' =>'C++','C#' =>'C#','Ruby' =>'Ruby','Java script' =>'Java script','Python' =>'Python', 'Type cript' =>'Type cript', 'VBA' =>'VBA', 'VB' =>'VB', 'Scala' =>'Scala', 'Kotolin' =>'Kotolin','Swift' =>'Swift'])}}</td></tr>
    <tr><th>使用したいサービス</th><td>{{Form::select('serveres', ['SAKURA' => 'SAKURA', 'CONOHA' => 'CONOHA', 'AZURE' =>'AZURE', 'AWS' =>'AWS','GCP' =>'GCP'])}}</td></tr>
-   <tr><th>使用しているOS</th><td>{{Form::select('os', ['WINDOWS' => 'WINDOWS', 'MACBOOK' => 'MACBOOK', 'Ubuntu' =>'Ubuntu', 'Centos7' =>'Centos7','Soralis' =>'Soralis','Linuxmint' =>'Linuxmint'])}}</td></tr>
+   <tr><th>使用したいOS</th><td>{{Form::select('os', ['WINDOWS' => 'WINDOWS', 'MACBOOK' => 'MACBOOK', 'Ubuntu' =>'Ubuntu', 'Centos7' =>'Centos7','Soralis' =>'Soralis','Linuxmint' =>'Linuxmint'])}}</td></tr>
      <input type="submit" value="押して下さい！！">
   </form>
  </table>
